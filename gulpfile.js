3810aeec29dd64e9103b9cb7f2510c841248715a
Navishkar Rao <raonav@outlook.com>
2015-06-13 21:29:08 +1200

    using gulp to build

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+//var nwBuilder = require('node-webkit-builder');
+var builder = require('gulp-node-webkit-builder');
+
+gulp.task('clean', function(){
+    return gulp.src('dist', {read: false})
+        .pipe(clean({force: true}));
+});
+
+gulp.task('build', ['clean'], function(){
+    return gulp.src('./src/**/**')
+        .pipe(builder({
+            platforms: ['osx64'],
+            buildDir: './dist/builds',
+            cacheDir: './dist/cache',
+            macIcns: './resources/icon.icns'
+        }));
+});
