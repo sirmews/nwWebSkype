5e5e172d9125f37de02ce58f3c2c0e2d06b85a41
Navishkar Rao <raonav@outlook.com>
2015-06-13 14:47:24 +1200

    adding windows builds to the build script

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 var NwBuilder = require('node-webkit-builder');
 var nw = new NwBuilder({
     files: './**/**', // use the glob format
-    platforms: ['osx64'],
+    platforms: ['osx', 'win'],
     buildDir: './dist/builds',
     cacheDir: './dist/cache',
     macIcns: './resources/icon.icns'
